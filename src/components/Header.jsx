9967ed4eea0210157aac46d7894fa5a77630f9a6
Chu A. <chu.amk22@gmail.com>
2025-11-25 13:25:37 +0100

    feat(ui): Implement the Header component
    app logo navigates back to the HomePage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,37 @@
 import { useNavigate } from "react-router-dom";
 import "../index.css";
-import NavBar from "./NavBar";
+import CategoryCard from "./CategoryCard";
 
-export default function Header() {
+export function AppBar() {
   const navigate = useNavigate();
   return (
-    <div>
-      <NavBar />
-      <div className="flex justify-center items-center h-90">
-        <main className="flex flex-col gap-8 items-center">
+    <header className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mt-6 mb-8 px-4 max-w-6xl mx-auto">
+      <a
+        className="cursor-pointer"
+        onClick={() => navigate("/")}
+        aria-label="Go to homepage"
+      >
+        <img src="../quiz-icon.svg" alt="Quiz Icon" className="w-auto h-16" />
+      </a>
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-amber-600 text-center">
+        The Trivia Game
+      </h1>
+
+      <div className="hidden sm:block w-16"></div>
+    </header>
+  );
+}
+
+export default function Header() {
+  return (
+    <>
+      <AppBar />
+      <div className="flex justify-center items-center ">
+        <main className="flex flex-col gap-8 items-center mt-4">
           <h1 className="text-4xl">Welcome to The Trivia Quiz!</h1>
-          <p>10 Questions to test your Trivia Knowledge</p>
-          <button
-            onClick={() => navigate("/quiz")}
-            type="button"
-            className="bg-sky-500 px-8 py-4 rounded-lg hover:bg-sky-700 cursor-pointer"
-          >
-            Lets Start
-          </button>
+          <CategoryCard />
         </main>
       </div>
-    </div>
+    </>
   );
 }
