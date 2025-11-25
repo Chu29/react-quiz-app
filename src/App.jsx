4836960c32d9e7dca6eb3236780fbfd0c6f0fbf0
Chu A. <chu.amk22@gmail.com>
2025-11-25 13:16:01 +0100

    feat: setup page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import QuizPage from "./pages/QuizPage";
+import ResultPage from "./pages/ResultPage";
+
 function App() {
   return (
-    <>
-      <div>APP</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage}></Route>
+        <Route path="/quiz-page/:category" Component={QuizPage}></Route>
+        <Route path="/quiz-page/results-page" Component={ResultPage}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
