5399adeaf6b545819d29b3fdaf375584f676f7ce
Chu A. <chu.amk22@gmail.com>
2025-11-25 13:32:35 +0100

    chore: setup utils folder
    create a constants.js file to store constants

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const BASE_URL = "https://the-trivia-api.com";
+
+export const CATEGORIES = [
+  {
+    id: "arts",
+    label: "Arts & Literature",
+  },
+  { id: "movies", label: "Film & TV" },
+  { id: "food_and_drink", label: "Food & Drink" },
+  { id: "history", label: "History" },
+  { id: "general_knowledge", label: "General Knowledge" },
+  { id: "geography", label: "Geography" },
+  { id: "music", label: "Music" },
+  { id: "science", label: "Science" },
+  { id: "society_and_culture", label: "Society & Culture" },
+  { id: "sport_and_leisure", label: "Sport & Leisure" },
+];
